6d91fd42fdcddd06f6522efcd8b16431d9633dd2
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-07-15 13:27:42 +0530

    testing server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log("Server running");
+})
