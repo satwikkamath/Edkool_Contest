b4e1fe3ca717e514a03469f5b359fd66a85d1f5c
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-07-16 21:15:40 +0530

    added transition

--- public/script.js
+++ public/script.js
@@ -1,4 +1,35 @@
+function reveal1() {
+    let r1 = document.querySelectorAll(".reveal1");
+    for (i = 0; i < r1.length; i++) {
+        let wh = window.innerHeight;     /*wh= window height */
+        let th = r1[i].getBoundingClientRect().top;   /*th= top height */
+        let vh = 150;    /*view height  */
+        if (th < wh - vh) {
+            r1[i].classList.add("active1");
+        }
+        else {
+            r1[i].classList.remove("active1");
+        }
+    }
+}
 
+function reveal2() {
+    let r2 = document.querySelectorAll(".reveal2");
+    for (i = 0; i < r2.length; i++) {
+        let wh = window.innerHeight;
+        let th = r2[i].getBoundingClientRect().top;
+        let vh = 150;
+        if (th < wh - vh) {
+            r2[i].classList.add("active2");
+        }
+        else {
+            r2[i].classList.remove("active2");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal1);
+window.addEventListener("scroll", reveal2);
 
 const getBookList = async (sendData) => {
     let options = {
@@ -64,7 +95,7 @@ document.getElementById("button-addon2").addEventListener('click', function () {
 
             result1 += `
             
-            <div class="col-sm-12 col-md-6 col-lg-4 my-3 d-flex align-items-stretch">
+            <div class="col-sm-12 col-md-6 col-lg-4 my-3 d-flex align-items-stretch reveal1">
                                 <div class="card text-center">
                                     <img class="mx-auto mt-4"
                                         src="${imgurl}"
@@ -112,7 +143,7 @@ document.getElementById("button-addon2").addEventListener('click', function () {
 
             result2 += `
             
-            <div class="col-sm-12 col-md-6 col-lg-4 my-3 d-flex align-items-stretch">
+            <div class="col-sm-12 col-md-6 col-lg-4 my-3 d-flex align-items-stretch reveal1">
                                 <div class="card text-center">
                                     <img class="mx-auto mt-4"
                                         src="${imgurl}"
@@ -160,7 +191,7 @@ document.getElementById("button-addon2").addEventListener('click', function () {
 
             result3 += `
             
-            <div class="col-sm-12 col-md-6 col-lg-4 my-3 d-flex align-items-stretch">
+            <div class="col-sm-12 col-md-6 col-lg-4 my-3 d-flex align-items-stretch reveal1">
                                 <div class="card text-center">
                                     <img class="mx-auto mt-4"
                                         src="${imgurl}"
@@ -276,4 +307,6 @@ function rightArrow() {
         pagNo1();
     }
 }
+
+
 
