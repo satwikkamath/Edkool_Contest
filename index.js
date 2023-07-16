fe4975d972fc04e28989eaf36e054741502d38a9
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-07-16 13:06:49 +0530

    updated user after login page

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const mongoose = require('mongoose');
 const https = require('https');
 const ejs = require('ejs');
 
+mongoose.connect("mongodb+srv://satwikroopa:Roopa70263@fruitdb.8sxipgz.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true });
+
+
+const bookUserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    phoneNumber: Number,
+})
+
+const BookUser = mongoose.model("bookUser", bookUserSchema);
+
 const app = express();
 app.use(express.json());
 
@@ -14,6 +26,68 @@ app.set("view engine", "ejs");
 
 app.get("/",function(req,res){
     res.render("home");
+})
+
+app.get("/loginFromMain",function(req,res){
+    res.render("login",{text:false,passwordFail:false,notFound:false});
+})
+
+app.get("/signupFromMain",function(req,res){
+    res.render("signup",{error:false});
+})
+
+
+
+app.post("/userSignUp", function (req, res) {
+    const receivedName = req.body.name;
+    
+    const receivedEmail = req.body.email;
+    
+    const receivedPhno = req.body.phno;
+
+    const receivedPswd = req.body.password;
+    const cpswd = req.body.cpassword;
+    if (receivedPswd != cpswd) {
+        res.render("signup", { error: true });  // if password and confirm password wont match
+    }
+    else {
+        BookUser.findOne({ email: receivedEmail }).then(function (data) {
+            if (data) {
+                res.render("userLogin", { text: "Account already exists with this Email", passwordFail: false, notFound: false });     // if an account already exists while signing up
+            }
+            else {
+                const user = new BookUser({
+                    name: receivedName,
+                    email: receivedEmail,                // if no account exists, new account is created 
+                    password: receivedPswd,
+                    phoneNumber: receivedPhno
+                })
+                user.save();
+                res.render("login", { text: "Your account was succesfully created", passwordFail: false, notFound: false });
+            }
+        })
+    }
+})
+
+app.post("/userLogin", function (req, res) {
+    const email = req.body.email;
+    const pswd = req.body.password;
+    BookUser.findOne({ email: email }).then(function (user) {
+        if (user) {
+            
+            if (pswd === user.password) {
+                res.render("user",{userName:user.name});  // if password matched
+            }
+            else {
+                res.render("login", { passwordFail: true, text: false, notFound: false });  // if password not matched
+            }
+        }
+        else {
+            res.render("login", { passwordFail: false, text: false, notFound: true });  // if password not matched
+        }
+    }).catch(function (err) {
+        res.send(err);
+    })
 })
 app.post("/getBooks",function(req,res){
     const bookName = req.body.title;
--- public/script.js
+++ public/script.js
@@ -1,33 +1,37 @@
-// const axios = require('axios');
 
-// const getBookList = async (sendData) => {
-//     let options = {
-//             method: "POST",
-//             headers: {
-//                     "Content-type": "application/json"
-//             },
-//             body: JSON.stringify(sendData),
-//     }
-//     let p = await fetch('/getBooks', options)
-//     let response = await p.json()
-//     return response
-// }
 
-// document.getElementById("btn").addEventListener('click', function () {
-//     const bookName = document.getElementById("bookName").value;
-//     // console.log(bookName);
-//     const mainFunc = async () => {
-//         let sendData = {
-//             title: bookName,
+const getBookList = async (sendData) => {
+    let options = {
+            method: "POST",
+            headers: {
+                    "Content-type": "application/json"
+            },
+            body: JSON.stringify(sendData),
+    }
+    let p = await fetch('/getBooks', options)
+    let response = await p.json()
+    return response
+}
 
-//         }
-//         let bookList = await getBookList(sendData)
-//         console.log(bookList)
+document.getElementById("btn").addEventListener('click', function () {
+    const bookName = document.getElementById("bookName").value;
+    const res= document.getElementById("res");
+    let string ="";
+    // console.log(bookName);
+    const mainFunc = async () => {
+        let sendData = {
+            title: bookName,
 
-//     }
+        }
+        let bookList = await getBookList(sendData)
+        let pag1=[];
+        let pag2=[];
+        let pag3=[];
 
-//     mainFunc()
-// })
+    }
+
+    mainFunc()
+})
 
 
 
