ee84a9eebd8561486f3044b15873fc16bd461b74
satwikkamath <107809929+satwikkamath@users.noreply.github.com>
2023-07-16 12:00:09 +0530

    added faq,services, famous book

--- public/script.js
+++ public/script.js
@@ -1,30 +1,33 @@
 // const axios = require('axios');
 
-const getBookList = async (sendData) => {
-    let options = {
-            method: "POST",
-            headers: {
-                    "Content-type": "application/json"
-            },
-            body: JSON.stringify(sendData),
-    }
-    let p = await fetch('/getBooks', options)
-    let response = await p.json()
-    return response
-}
+// const getBookList = async (sendData) => {
+//     let options = {
+//             method: "POST",
+//             headers: {
+//                     "Content-type": "application/json"
+//             },
+//             body: JSON.stringify(sendData),
+//     }
+//     let p = await fetch('/getBooks', options)
+//     let response = await p.json()
+//     return response
+// }
 
-document.getElementById("btn").addEventListener('click', function () {
-    const bookName = document.getElementById("bookName").value;
-    // console.log(bookName);
-    const mainFunc = async () => {
-        let sendData = {
-            title: bookName,
+// document.getElementById("btn").addEventListener('click', function () {
+//     const bookName = document.getElementById("bookName").value;
+//     // console.log(bookName);
+//     const mainFunc = async () => {
+//         let sendData = {
+//             title: bookName,
 
-        }
-        let bookList = await getBookList(sendData)
-        console.log(bookList)
-        
-    }
+//         }
+//         let bookList = await getBookList(sendData)
+//         console.log(bookList)
 
-    mainFunc()
-})
+//     }
+
+//     mainFunc()
+// })
+
+
+
